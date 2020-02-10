604221e312595359b116ebd5e4866f8f6660ed81
Murali Krishna Kuravi <murali.kuravi@sonata-software.com>
2020-02-10 20:15:38 +0530

    dfs

--- src/com/test/net/Test.java
+++ src/com/test/net/Test.java
@@ -3,7 +3,7 @@ package com.test.net;
 import java.sql.*;  
 import java.util.*;
 class Test{  
-	public static void main(String args[]){
+	public static void main(String[] args){
 		System.out.println("text=====calling");
 		for(int i=10;i>0;i--) {
 			System.out.println("Hello "+i);	
