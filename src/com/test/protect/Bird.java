239e43e18f5b26db1dfadd764d0facb8b99a2a98
Murali Krishna Kuravi <murali.kuravi@sonata-software.com>
2020-02-10 20:23:45 +0530

    Deleted for testing

--- src/com/test/protect/Bird.java
+++
@@ -1,8 +0,0 @@
-package com.test.protect;
-
-public class Bird {
-	protected static String text="float in water";
-	protected static void  floatinWater() {
-		System.out.println("text==="+text);
-	}
-}
