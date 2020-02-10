06db639006a0b5c8f04b872fd5c26906a12229f5
Murali Krishna Kuravi <murali.kuravi@sonata-software.com>
2020-02-10 20:31:20 +0530

    added public

--- src/com/test/net/Test.java
+++ src/com/test/net/Test.java
@@ -2,12 +2,9 @@ package com.test.net;
 
 import java.sql.*;  
 import java.util.*;
-class Test{  
-	public static void main(String[] args){
-		System.out.println("text=====calling");
-		for(int i=10;i>0;i--) {
-			System.out.println("Hello "+i);	
-		}
+public class Test{  
+	public static void main(String args[]){
 		
+		System.out.println("text=====calling");
 	}  
 }  
